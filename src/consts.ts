939436ab41bbc079665672df962c9d3d266a4086
Jake Bailey <jake@jaketbailey.co.uk>
2024-08-25 20:08:27 +0100

    colours

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "HB Media";
-export const SITE_DESCRIPTION = "HB Media!";
+export const SITE_TITLE = "HB Film & Media";
+export const SITE_DESCRIPTION = "HB Film & Media!";
